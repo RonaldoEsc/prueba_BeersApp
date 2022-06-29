9c6957043f130814c28adaa6c3049ffed42c1be7
Hugo Escamilla <hescamilla@iplace.net>
2022-06-29 16:17:44 -0500

    creacion y finalizacion de la vista de recyclerview con su adapter solo falta que dependa de informacion de sql para guardar favoritos y las notas.

--- app/src/main/java/net/hrec/pruebatecnica/usecases/common/BeersListAdapter.kt
+++ app/src/main/java/net/hrec/pruebatecnica/usecases/common/BeersListAdapter.kt
@@ -4,14 +4,24 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import net.hrec.pruebatecnica.R
 import net.hrec.pruebatecnica.databinding.BeerViewHolderBinding
 import net.hrec.pruebatecnica.model.BeersResponse
 
 class BeersListAdapter(val beer: (Int) -> Unit): RecyclerView.Adapter<BeersListAdapter.BeerViewHolder>() {
     private var listBeers = mutableListOf<BeersResponse>()
-    class BeerViewHolder(private val binding: BeerViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(beer: BeersResponse) {
+    private val favoriteSelectedList = mutableListOf<Int>()
+    class BeerViewHolder(binding: BeerViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
+        private val tvBeerName = binding.tvBeerName
+        private val tvBeerTagName = binding.tvBeerTagName
+        private val imgBeer = binding.imgBeer
+        val ibFavorite = binding.ibFavorite
 
+        fun bind(beer: BeersResponse) {
+            tvBeerName.text = beer.name
+            tvBeerTagName.text = beer.tagline
+            Picasso.get().load(beer.imageUrl).placeholder(R.drawable.progress_animation).into(imgBeer)
         }
     }
 
@@ -30,6 +40,41 @@ class BeersListAdapter(val beer: (Int) -> Unit): RecyclerView.Adapter<BeersListA
     override fun onBindViewHolder(holder: BeerViewHolder, position: Int) {
         val beer = listBeers[position]
         holder.bind(beer)
+        if (favoriteSelectedList.isEmpty()) {
+            holder.ibFavorite.setImageResource(R.drawable.ic_favorite_border)
+        } else {
+            var existPositionInList = false
+            favoriteSelectedList.forEach {
+               if (it == position) {
+                   existPositionInList = true
+                   return@forEach
+               }
+            }
+            if (existPositionInList) {
+                holder.ibFavorite.setImageResource(R.drawable.ic_favorite)
+            } else {
+                holder.ibFavorite.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+        holder.ibFavorite.setOnClickListener {
+            holder.ibFavorite.setImageResource(R.drawable.ic_favorite)
+            if (favoriteSelectedList.isEmpty()) {
+                favoriteSelectedList.add(position)
+            } else {
+                var isInList = false
+                favoriteSelectedList.forEach {
+                    if (it == position) {
+                        isInList = true
+                    }
+                }
+                if (isInList) {
+                    favoriteSelectedList.remove(position)
+                } else {
+                    favoriteSelectedList.add(position)
+                }
+            }
+            notifyDataSetChanged()
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/net/hrec/pruebatecnica/usecases/home/HomeActivity.kt
+++ app/src/main/java/net/hrec/pruebatecnica/usecases/home/HomeActivity.kt
@@ -10,24 +10,9 @@ import net.hrec.pruebatecnica.usecases.common.BeersListAdapter
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var viewModel: HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        val llm = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        llm.orientation = LinearLayoutManager.VERTICAL
-        binding.rvBeers.layoutManager = llm
-        binding.rvBeers.adapter = BeersListAdapter{ id ->
-
-        }
-
-        viewModel.getBeers(1, 80)
-
-        viewModel.beersList.observe(this) { list ->
-            (binding.rvBeers.adapter as BeersListAdapter).setData(list.toMutableList())
-        }
-
     }
 }
